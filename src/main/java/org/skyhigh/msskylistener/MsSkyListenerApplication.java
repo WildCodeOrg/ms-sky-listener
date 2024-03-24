8901dff3c4f8acd6b971c0402f9bacc2d71f3a7a
Daniil Popov <popovdanin@gmail.com>
2024-03-24 12:32:19 +0300

    Initial commit

---
+++ src/main/java/org/skyhigh/msskylistener/MsSkyListenerApplication.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msskylistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSkyListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSkyListenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skyhigh/msskylistener/MsSkyListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msskylistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSkyListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
